746ade5851aca9390c9b5c122e1cb82db3197864
Justin Mepham <justin0mepham@gmail.com>
2020-05-10 12:24:14 -0600

    Finished, The only issues is an issues with the highlighting of my rps images but that's because I had issues with photo editing, not coding

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector("score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -14,11 +14,46 @@ function getComputerChoice() {
     return choices[random_num];
 }
 
+function convertToWord(letter) {
+    if (letter === "r") return "Rock";
+    if (letter === "p") return "Paper";
+    if (letter === "s") return "Scissors";
+}
+
+function win(userChoice, computerChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    result_p.innerHTML = convertToWord(userChoice) + " beats " + convertToWord(computerChoice) + ". You win!"
+}
+function lose(userChoice, computerChoice) {
+    computerScore++;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = convertToWord(computerChoice) + " beats " + convertToWord(userChoice) + ". You lose!"
+}
+function draw(userChoice, computerChoice) {
+    result_p.innerHTML = "You both chose " + convertToWord(userChoice) + ". It's a draw!"
+}
+
 
 function game(userChoice) {
     const computerChoice = getComputerChoice();
-    console.log(computerChoice);
-    console.log(userChoice)
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win(userChoice, computerChoice);
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            lose(userChoice, computerChoice);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw(userChoice, computerChoice);
+            break;
+    }
 }
 
 
